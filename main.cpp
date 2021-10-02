f43ddbba6e02c098eb03308c76145de4c1894b44
Gerson Soares da Silva <86379313+GersonSoaresdaSilva@users.noreply.github.com>
2021-10-02 18:46:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,2 @@
+/*Faça o código fonte estruturado de algoritmo que contenha uma estrutura (STRUCT) chamada CLIENTE criada para armazenar até 10 elementos, com os atributos NOME, SALÁRIO e TELEFONE. O programa será comandado por um menu com select case com as seguintes opções: no Case 1 Ler Dados, case 2 Listar Registros, case 3 classificar por nome, case 4 busca sequencial por NOME, case 5 busca binária por nome e case 6 sair*/
+
